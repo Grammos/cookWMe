185b58cf45b9fdcb9fc4a040c8da28217b468c67
Grammos <kamberigramos@gmail.com>
2017-06-28 20:45:03 +0200

    Added content to the Recipes Component and outputting a List of Recipes with ngFor.

--- cook-w-me/src/app/recipes/recipes-list/recipes-list.component.ts
+++ cook-w-me/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-	// added the array of the recipes
-	recipes = [];
+
+	// added the array of the recipes through the Recipe of our model
+	recipes: Recipe[] = [
+	  new Recipe('Test recipe', 'Test', 'https://pbs.twimg.com/profile_images/529682367170899968/lffGtykQ.png'),
+	  new Recipe('Test recipe', 'Test', 'https://pbs.twimg.com/profile_images/529682367170899968/lffGtykQ.png')
+	];
 
 
   constructor() { }
