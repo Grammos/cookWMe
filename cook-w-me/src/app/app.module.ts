a3dc3b2a943cc33814eb5aa5ab04998d4873bd1b
Grammos <kamberigramos@gmail.com>
2017-06-28 13:32:04 +0200

    Added Components of Recipes(detail, list-item)

--- cook-w-me/src/app/app.module.ts
+++ cook-w-me/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesDetailComponent,
+    RecipesListComponent,
+    RecipesItemComponent
   ],
   imports: [
     BrowserModule
