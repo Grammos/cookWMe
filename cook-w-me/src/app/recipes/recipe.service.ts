2b5d9b56152dcba01d3f743ccf82559e9f65a220
Grammos <kamberigramos@gmail.com>
2017-07-08 12:10:40 +0200

    Using a Service for cross-component communication on the Recipe.

--- cook-w-me/src/app/recipes/recipe.service.ts
+++ cook-w-me/src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
 	  new Recipe('Test recipe', 'Test', 'https://pbs.twimg.com/profile_images/529682367170899968/lffGtykQ.png'),
 	  new Recipe('Test Recipe Kosovo', 'Test', 'https://pbs.twimg.com/profile_images/529682367170899968/lffGtykQ.png')
