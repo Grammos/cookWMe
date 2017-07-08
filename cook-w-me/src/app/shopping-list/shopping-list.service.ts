d95c67657ada221c586a140d13c8e66f508032d3
Grammos <kamberigramos@gmail.com>
2017-07-08 13:51:14 +0200

    Added the ShoppingList Service. Also, changed the type of the button.

--- cook-w-me/src/app/shopping-list/shopping-list.service.ts
+++ cook-w-me/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,17 @@
+import { Ingredient } from '../shared/ingredient.model'
+
 export class ShoppingListService {
-	
+  private ingredients: Ingredient[]  = [
+  	new Ingredient('Apples', 5),
+  	new Ingredient('Oranges', 5)
+  ];
+
+  getIngredients() {
+  	return this.ingredients.slice();
+  }
+
+  addIngredients(ingredient: Ingredient) {
+  	this.ingredients.push(ingredient);
+  }
+
 }
